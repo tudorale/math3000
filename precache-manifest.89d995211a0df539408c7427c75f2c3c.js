6bf43ae576131cd9743e40e56ccc3f72e713e53d
Tudor Alexandru <tudorale2005@gamil.com>
2020-07-06 13:22:42 -1200

    Updates

---
+++ precache-manifest.89d995211a0df539408c7427c75f2c3c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "151af16193f8234ef2675cad92cbe0ed",
+    "url": "/math3000/index.html"
+  },
+  {
+    "revision": "4f96aad4410c660c6880",
+    "url": "/math3000/static/css/main.7b0d69fc.chunk.css"
+  },
+  {
+    "revision": "23cc87ad06190fc74be4",
+    "url": "/math3000/static/js/2.e7efe4f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/math3000/static/js/2.e7efe4f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f96aad4410c660c6880",
+    "url": "/math3000/static/js/main.0fd3df9a.chunk.js"
+  },
+  {
+    "revision": "b8baeb3a05cc6b3e358e",
+    "url": "/math3000/static/js/runtime-main.775fbdc5.js"
+  },
+  {
+    "revision": "839ac8a9798f9a435f4ddb387cdf928e",
+    "url": "/math3000/static/media/robot.839ac8a9.svg"
+  },
+  {
+    "revision": "3ed2b067794f91d5fa929cadb29a09b8",
+    "url": "/math3000/static/media/wifi.3ed2b067.svg"
+  }
+]);
